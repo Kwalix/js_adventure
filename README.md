1d1f220f95235058797c5da97f0142c4519a1cdd
Kwalix <lyk4n@protonmail.com>
2020-04-16 21:43:11 +0200

    reformatage de game.js et mise a jour de README.md

--- README.md
+++ README.md
@@ -3,11 +3,15 @@
 JS Adventure, is a simple games code in JavaScript
 
 
-#### Launching new game :
+#### Game manipulation :
 
 for launch a new party type this command :
 
 ```javascript
 newGame();
 ```
-> Code in development /!\
+The life and attack variables are fixed for the moment (I will add a future functionality allowing to energize this)
+For the moment the only way to change them is to change them yourself in game.js,
+
+> The code is currently in development, 
+> the game may have missing features or bugs.
--- game.js
+++ game.js
@@ -22,25 +22,27 @@ function newGame() {
     alert('Oh !\n a monster has just appeared in front of you !');
     alert('You have to face it');
     while (monster.health > 0) {
-        let choice = parseInt(prompt('What do you want to do ?\n 1: Attack him\n 2: Run Away (experimental)'));
+        let choice = parseInt(prompt('What do you want to do ?\n 1: Attack him\n 2: increase your attack of 5'));
         switch (choice) {
             case 1:
-                if (isNaN(choice)) {
-                    console.error('You must enter a number :(');
-                    return false;
-                } else {
-                    alert('You attack the ' + monster.name);
-                    monster.health -= you.atk;
-                    alert('You have inflicted ' + you.atk + ' on ' + monster.name);
-                }
+                alert('You attack the ' + monster.name);
+                monster.health -= you.atk;
+                alert('You have inflicted ' + you.atk + ' on ' + monster.name);
                 break;
             case 2:
-                continue;
+                you.atk += 5;
+                alert('you increased your power\n your power: ' + you.atk);
+                break;
             default:
                 alert('you have chosen nothing, the ' + monster.name + ' attacks you');
                 you.health -= monster.atk;
                 alert(monster.name + ' inflict you ' + monster.atk + ' of damage');
         }
+        // Monster attack you after attacked him
+        alert(monster.name + ' attack you');
+        you.health -= monster.atk;
+        alert(monster.name + ' inflict you ' + monster.atk + ' of damage');
+        // if your life drops to 0
         if (you.health <= 0) {
             alert('Game Over\n your life fell to 0');
             break;
@@ -48,6 +50,10 @@ function newGame() {
             alert('GG, you defeated the troll ^^');
             break;
         }
+    }
+    if (window.confirm('play again ?'))
+    {
+        newGame();
     }
     alert('Thanks for playing: p\n Developed by kwalix <3');
     return "thx for playing";
